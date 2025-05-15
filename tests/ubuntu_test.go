d333bd30504b2a5bceeb1d7418846af2eff6d5b8
Anthony <antyung47@gmail.com>
2025-05-15 12:35:10 +0000

    Add testcontainers

---
+++ tests/ubuntu_test.go
@@ -0,0 +1,57 @@
+package tests
+
+import (
+	"context"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+)
+
+var Ubuntu = struct {
+	AWS_DEFAULT_REGION string
+	AWS_ECR_PUBLIC_URI string
+	DOCKER_IMAGE_GROUP string
+	DOCKER_IMAGE       string
+	DOCKER_IMAGE_TAG   string
+}{
+	AWS_DEFAULT_REGION: "us-east-1",
+	AWS_ECR_PUBLIC_URI: "public.ecr.aws/f7i0q1v8",
+	DOCKER_IMAGE_GROUP: "base",
+	DOCKER_IMAGE:       "ubuntu",
+	DOCKER_IMAGE_TAG:   "latest",
+}
+
+func TestContainersGoPullUbuntu(t *testing.T) {
+	ctx := context.Background()
+	container, e := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Image: Ubuntu.AWS_ECR_PUBLIC_URI + "/" + Ubuntu.DOCKER_IMAGE_GROUP + "/" + Ubuntu.DOCKER_IMAGE + ":" + Ubuntu.DOCKER_IMAGE_TAG,
+		},
+	})
+	require.NoError(t, e)
+	defer func() {
+		_ = container.Terminate(ctx)
+	}()
+}
+
+func TestContainersGoExecUbuntu(t *testing.T) {
+	ctx := context.Background()
+	container, e := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Image: Ubuntu.AWS_ECR_PUBLIC_URI + "/" + Ubuntu.DOCKER_IMAGE_GROUP + "/" + Ubuntu.DOCKER_IMAGE + ":" + Ubuntu.DOCKER_IMAGE_TAG,
+			Cmd:   []string{"echo", "Hello, World!"},
+		},
+		Started: true,
+	})
+	require.NoError(t, e)
+	defer func() {
+		_ = container.Terminate(ctx)
+	}()
+	logs, e := container.Logs(ctx)
+	require.NoError(t, e)
+	output, e := io.ReadAll(logs)
+	require.NoError(t, e)
+	require.Contains(t, string(output), "Hello, World!", "Expected output not found")
+}
