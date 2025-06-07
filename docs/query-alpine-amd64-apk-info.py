6b616030483b7a6945b91fc3cbcc8e6db9f5f3eb
antyung47 <antyung47@gmail.com>
2025-06-07 15:20:59 +0800

    apk info update

---
+++ docs/query-alpine-amd64-apk-info.py
@@ -0,0 +1,23 @@
+import docker
+
+client = docker.from_env()
+
+image_name = "public.ecr.aws/dev1-sg/base/alpine:latest"
+platform = "linux/amd64"
+
+print(f"Pulling image: {image_name} for platform {platform}")
+client.images.pull(image_name, platform=platform)
+
+output = client.containers.run(
+    image=image_name,
+    command="apk info -v",
+    remove=True,
+    platform=platform
+)
+
+output_file = "../src/alpine/amd64_apk_info.md"
+
+with open(output_file, "wb") as f:
+    f.write(output)
+
+print(f"Output saved to {output_file}")
