8e219bd311de4432fd6396ccb74bd03fc39f43ff
antyung47 <antyung47@gmail.com>
2025-08-10 17:18:19 +0800

    fix jre size

--- src/java/tags.sh
+++ src/java/tags.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-version=$(sed -n 's|.*eclipse-temurin:\([0-9.]*\)-alpine-\([0-9.]*\).*|\1-\2|p' Dockerfile)
+version=$(sed -n 's|.*eclipse-temurin:\([0-9.]*\)-jre-alpine-\([0-9.]*\).*|\1-\2|p' Dockerfile)
 
 if [ -n "$version" ]; then
     echo "$version"
