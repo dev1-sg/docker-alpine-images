d35267807a3a64ff750e55457db79221036138cd
antyung47 <antyung47@gmail.com>
2025-08-14 00:20:37 +0800

    update

--- src/java/tags.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-version=$(sed -n 's|.*eclipse-temurin:\([0-9.]*\)-jre-alpine-\([0-9.]*\).*|\1-\2|p' Dockerfile)
-
-if [ -n "$version" ]; then
-    echo "$version"
-else
-    echo "latest"
-fi
